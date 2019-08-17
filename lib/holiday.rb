b7252b5740297370574680e3ac8a151a98dbc1ae
David Ingram <david@ingramaviation.com>
2019-08-17 17:35:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash. map do |holiday
+  holiday_hash. map do |holiday|
     holiday.map do |holiday, supply|
       holiday if supply.include?("BBQ")
     end
